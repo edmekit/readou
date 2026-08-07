0fe21f4288e1dbc6164f11203f0d6933dbde0683
edmekit <mekitpekit.eggmarc@gmail.com>
2026-08-07 08:51:00 +0800

    added filtering

--- readou-frontend/src/components/StoryCard.jsx
+++ readou-frontend/src/components/StoryCard.jsx
@@ -1,10 +1,10 @@
 function StoryCard({ story, onClick }) {
     return (
-        <div className="flex flex-col border-2 border-black p-5 cursor-pointer"
+        <div className="flex flex-col border-2 border-black p-5 cursor-pointer bg-gray-300"
         onClick={onClick}> 
             <img className="w-64 h-96" 
             src={story.cover_url}/>
-            <h2>{story.title}</h2>
+            <h2 className="text-lg text-white">{story.title}</h2>
         </div>
     )
 }
--- readou-frontend/src/pages/Stories.jsx
+++ readou-frontend/src/pages/Stories.jsx
@@ -1,28 +1,46 @@
 import Navbar from "../components/Navbar";
 import StoryCard from "../components/StoryCard";
 import ReviewCard from "../components/ReviewCard";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useMemo } from "react";
 
 function Stories(){
     const [stories, setStories] = useState([])
     const [selectedStory, setSelectedStory] = useState(null);
+    const [search, setSearch] = useState('');
     useEffect(() => {
         fetch('http://127.0.0.1:8000/stories')
         .then(res => res.json())
         .then(data => setStories(data));
     }, []);
 
+    const filteredStories = useMemo(() => {
+        return stories.filter(story => story.title.toLowerCase().includes(search.toLowerCase()));
+    }, [stories, search]);
+
+    const visibleStories = search === "" ? stories : filteredStories;
+
     return (
         <div>
             <Navbar/>
-            <input type="text" placeholder="Search" 
+            <input type="text" value={search} 
+            onChange= {(e) => setSearch(e.target.value)} placeholder="Search" 
             className="border-1 border-black rounded-[20px] p-2 m-2 shadow-md" />
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
-                {stories.map((story) => (
-                    <StoryCard key={story.id} story={story}
-                    onClick={() => setSelectedStory(story)}/>
-                ))}
-            </div>
+
+            {
+                visibleStories.length > 0 ? 
+                (<div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
+                    {visibleStories.map((story) => (
+                        <StoryCard key={story.id} story={story}
+                        onClick={() => setSelectedStory(story)}/>
+                    ))}
+                </div>)
+                :
+                (<div className="flex flex-col justify-center items-center w-full h-130 text-gray-400 text-2xl">
+                    <h1>No stories found</h1>
+                </div>)
+            }
+
+
             {selectedStory && (
             <ReviewCard
                 story={selectedStory}
